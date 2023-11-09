a7cf543ac169d5304f4067094167cc7a7c3bf1c3
EC2 Default User <ec2-user@ip-172-31-16-124.ap-northeast-1.compute.internal>
2023-11-09 08:55:38 +0000

    [Finish]投稿一覧、ユーザ詳細のデザイン

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -6,18 +6,22 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
   end
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
-  
+
   def destroy
     post_image = PostImage.find(params[:id])
     post_image.destroy
